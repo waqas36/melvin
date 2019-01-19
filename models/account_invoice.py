ff94ede5b7f67a96391dbc185ab47ed6b033cec6
tabish turabi <tabishturabi@axiomworld.net>
2019-01-19 16:12:32 +0500

    tabish changes

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -4,9 +4,13 @@ from odoo import models, fields, api
 class ResCompany(models.Model):
     _inherit = "account.invoice"
 
-    project_code = fields.Char(string='Project Code', compute='_get_project_code')
+    project_code = fields.Char(string='Job Code', compute='_get_project_code')
 
     @api.depends('origin')
     def _get_project_code(self):
         sale_obj = self.env['sale.order'].search([('name', '=', self.origin)])
-        self.project_code = sale_obj.job_dashboard_id.name
+        purchase_obj = self.env['purchase.order'].search([('name', '=', self.origin)])
+        if sale_obj:
+            self.project_code = sale_obj.job_dashboard_id.name
+        else:
+            self.project_code = purchase_obj.job_id.name
